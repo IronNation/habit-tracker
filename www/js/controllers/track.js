c50d19e18f6895014d5276af613341dc6cf78862
Shawn Taylor <shawn@proximityviz.com>
2017-01-08 10:55:34 -0500

    Adds calendar picker

--- www/js/controllers/track.js
+++ www/js/controllers/track.js
@@ -1,24 +1,41 @@
 angular.module('starter.controllers')
-.controller('TrackCtrl', function($scope, $filter, $ionicModal, HabitsFactory, DatesFactory) {
+.controller('TrackCtrl', function($scope, $filter, $ionicModal, ionicDatePicker, HabitsFactory, DatesFactory) {
 
   // TODO: change "todayData" to "thisDayData", and use relevant date instead of 'today'
   var today = $filter('date')(new Date(), 'yyyy-MM-dd');
+  var selectedDay = today;
 
   $scope.toggleHabit = function(index) {
     console.log(index);
     // toggle the data
-    DatesFactory.toggleHabit(today, index);
+    DatesFactory.toggleHabit(selectedDay, index);
     // then update data for display
-    $scope.todayData = DatesFactory.getDataForDate(today);
+    $scope.todayData = DatesFactory.getDataForDate(selectedDay);
   };
-  $scope.tabTitle = today;
+  $scope.tabTitle = $filter('date')(selectedDay, 'mediumDate');
 
   $scope.habits = HabitsFactory.getHabits();
   $scope.noHabits = $scope.habits.length > 0 ? false : true;
-  $scope.todayData = DatesFactory.getDataForDate(today);
+  $scope.selectedDayData = DatesFactory.getDataForDate(selectedDay);
 
 
-  // MODAL
+  // CALENDAR MODAL
+  var pickerSettings = {
+    callback: function (val) {
+      console.log($filter('date')(val, 'yyyy-MM-dd'));
+      selectedDay = $filter('date')(val, 'yyyy-MM-dd');
+      $scope.selectedDayData = DatesFactory.getDataForDate(selectedDay);
+      $scope.tabTitle = $filter('date')(selectedDay, 'mediumDate');
+    },
+    templateType: 'modal'
+  };
+
+  $scope.openDatePicker = function() {
+    ionicDatePicker.openDatePicker(pickerSettings);
+  };
+
+
+  // NEW HABIT MODAL
   $scope.addHabit = function() {
     $scope.modal.show();
   };
